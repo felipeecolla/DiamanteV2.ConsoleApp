f98f8b769369390504f1c8341b8fc5078f85c204
Felipe <Felipe@DESKTOP-1VO2IEC>
2022-02-16 16:41:36 -0300

    Add project files.

---
+++ DiamanteV2.ConsoleApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace DiamanteV2.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] alfabetosArray = alfabeto.ToCharArray();
+            Console.WriteLine("Insira uma letra: ");
+            string letra = Console.ReadLine();
+            int linhas = alfabeto.IndexOf(letra);
+            int espacos = linhas;
+            int espacosMeio = 0;
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = espacos; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(alfabetosArray[i]);
+
+                for (int j = 0; j < espacosMeio; j++)
+                {
+                    Console.Write(" ");
+                }
+                if (i > 0)
+                {
+                    Console.Write(alfabetosArray[i]);
+                }
+
+                Console.WriteLine();
+
+                espacos--;
+                    espacosMeio += 2;
+
+
+            }
+            for (int i = linhas; i >= 0; i--)
+            {
+                for (int j = espacos; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(alfabetosArray[i]);
+
+                for (int j = 0; j < espacosMeio; j++)
+                {
+                    Console.Write(" ");
+                }
+                if (i > 0)
+                {
+                    Console.Write(alfabetosArray[i]);
+                }
+
+                Console.WriteLine();
+
+                espacos++;
+                espacosMeio -= 2;
+
+
+            }
+
+
+
+        }
+    }
+}
